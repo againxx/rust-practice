5ae92e2aa572d8528e473c9a11812145d02401c9
Xi Xia <againxx@mail.ustc.edu.cn>
2021-09-30 20:36:48 +0800

    Finish shopping list

--- nowcoder/shopping_list/src/main.rs
+++ nowcoder/shopping_list/src/main.rs
@@ -1,53 +1,69 @@
 use std::io;
 
 #[derive(Debug, Clone)]
-struct MainComponent {
+struct BaseProperty {
     price: u32,
     priority: u32,
-    attached_components: Vec<AttachedComponent>,
 }
 
 #[derive(Debug, Clone)]
-struct AttachedComponent {
-    price: u32,
-    priority: u32,
+struct CandidateObject {
+    base_property: BaseProperty,
+    attachments: Vec<BaseProperty>,
 }
 
-impl MainComponent {
-    fn get_all_combinations(&self) -> Vec<(u32, u32)> {
-        assert!(
-            self.attached_components.len() <= 2,
-            "Too many attached components"
-        );
-        match self.attached_components.len() {
-            0 => vec![(self.price, self.priority)],
-            1 => vec![
-                (self.price, self.priority),
-                (
-                    self.price + self.attached_components[0].price,
-                    self.priority + self.attached_components[0].priority,
-                ),
-            ],
-            _ => vec![
-                (self.price, self.priority),
-                (
-                    self.price + self.attached_components[0].price,
-                    self.priority + self.attached_components[0].priority,
-                ),
-                (
-                    self.price + self.attached_components[1].price,
-                    self.priority + self.attached_components[1].priority,
-                ),
-                (
-                    self.price
-                        + self.attached_components[0].price
-                        + self.attached_components[1].price,
-                    self.priority
-                        + self.attached_components[0].priority
-                        + self.attached_components[1].priority,
-                ),
-            ],
+impl BaseProperty {
+    fn new() -> Self {
+        Self {
+            price: 0,
+            priority: 0,
         }
+    }
+}
+
+impl CandidateObject {
+    fn new() -> Self {
+        Self {
+            base_property: BaseProperty::new(),
+            attachments: Vec::new(),
+        }
+    }
+
+    fn get_all_combinations(&self) -> Vec<(u32, u32)> {
+        assert!(self.attachments.len() <= 2, "Too many attached components");
+        let mut result = vec![(
+            self.base_property.price,
+            self.base_property.price * self.base_property.priority,
+        )];
+        match self.attachments.len() {
+            1 => result.push((
+                self.base_property.price + self.attachments[0].price,
+                self.base_property.price * self.base_property.priority
+                    + self.attachments[0].price * self.attachments[0].priority,
+            )),
+            2 => {
+                result.push((
+                    self.base_property.price + self.attachments[0].price,
+                    self.base_property.price * self.base_property.priority
+                        + self.attachments[0].price * self.attachments[0].priority,
+                ));
+                result.push((
+                    self.base_property.price + self.attachments[1].price,
+                    self.base_property.price * self.base_property.priority
+                        + self.attachments[1].price * self.attachments[1].priority,
+                ));
+                result.push((
+                    self.base_property.price
+                        + self.attachments[0].price
+                        + self.attachments[1].price,
+                    self.base_property.price * self.base_property.priority
+                        + self.attachments[0].price * self.attachments[0].priority
+                        + self.attachments[1].price * self.attachments[1].priority,
+                ));
+            }
+            _ => (),
+        }
+        result
     }
 }
 
@@ -63,8 +79,8 @@ fn main() {
     let total_money = inputs[0] / 10;
     let object_num = inputs[1];
 
-    let mut main_components: Vec<MainComponent> = Vec::new();
-    main_components.reserve(object_num as usize);
+    let mut candidate_objects: Vec<Option<CandidateObject>> = Vec::new();
+    candidate_objects.resize(object_num as usize, Some(CandidateObject::new()));
 
     for i in 0..object_num as usize {
         input_buffer.clear();
@@ -75,59 +91,46 @@ fn main() {
             .map(|s| s.parse::<u32>().unwrap())
             .collect();
         if inputs[2] == 0 {
-            // Main component
-            if i >= main_components.len() {
-                main_components.push(MainComponent {
-                    price: inputs[0] / 10,
-                    priority: inputs[1],
-                    attached_components: Vec::new(),
-                })
-            } else {
-                main_components[i].price = inputs[0] / 10;
-                main_components[i].priority = inputs[1];
-            }
+            candidate_objects[i].as_mut().unwrap().base_property.price = inputs[0] / 10;
+            candidate_objects[i]
+                .as_mut()
+                .unwrap()
+                .base_property
+                .priority = inputs[1];
         } else {
             // Attached component
-            if inputs[2] as usize > main_components.len() {
-                main_components.resize(
-                    inputs[2] as usize,
-                    MainComponent {
-                        price: 0,
-                        priority: 0,
-                        attached_components: Vec::new(),
-                    },
-                );
-            }
-            main_components[(inputs[2] - 1) as usize]
-                .attached_components
-                .push(AttachedComponent {
+            candidate_objects[i] = None;
+            candidate_objects[(inputs[2] - 1) as usize]
+                .as_mut()
+                .unwrap()
+                .attachments
+                .push(BaseProperty {
                     price: inputs[0] / 10,
                     priority: inputs[1],
-                })
+                });
         }
     }
 
-    println!("{:?}", main_components);
+    let mut previous_stage = vec![0; (total_money + 1) as usize];
+    let mut current_stage = vec![0; (total_money + 1) as usize];
 
-    let mut previous_stage = vec![0; total_money as usize];
-    let mut current_stage = vec![0; total_money as usize];
-
-    for main_component in main_components.iter() {
-        for i in (main_component.price - 1)..total_money {
-            let combinations = main_component.get_all_combinations();
-            for combination in combinations {
-                if combination.0 - 1 <= i {
-                    let candidates = [
-                        previous_stage[i as usize],
-                        current_stage[i as usize],
-                        previous_stage[(i + 1 - combination.0) as usize]
-                            + combination.0 * combination.1,
-                    ];
-                    current_stage[i as usize] = *candidates.iter().max().unwrap();
+    for candidate in candidate_objects.iter() {
+        if let Some(obj) = candidate {
+            let combinations = obj.get_all_combinations();
+            for i in (obj.base_property.price)..total_money + 1 {
+                for combination in combinations.iter() {
+                    if combination.0 <= i {
+                        let candidates = [
+                            previous_stage[i as usize],
+                            current_stage[i as usize],
+                            previous_stage[(i - combination.0) as usize] + combination.1,
+                        ];
+                        current_stage[i as usize] = *candidates.iter().max().unwrap();
+                    }
                 }
             }
+            previous_stage = current_stage.clone();
         }
-        previous_stage = current_stage.clone();
     }
     println!("{}", current_stage.last().unwrap() * 10);
 }
