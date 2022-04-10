e366800f639b4dbf5af972ebd505a381891878ad
Xi Xia <againxx@mail.ustc.edu.cn>
2022-04-10 20:41:11 +0800

    Add http server in thumbor

--- rust_chentian/thumbor/src/main.rs
+++ rust_chentian/thumbor/src/main.rs
@@ -1,5 +1,47 @@
+use axum::{extract::Path, handler::get, http::StatusCode, Router};
+use percent_encoding::{percent_encode, percent_decode_str, NON_ALPHANUMERIC};
+use serde::Deserialize;
+
 mod pb;
 
-fn main() {
-    println!("Hello, world!");
+use pb::*;
+
+#[derive(Deserialize)]
+struct Params {
+    spec: String,
+    url: String,
+}
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt::init();
+
+    let app = Router::new()
+        .route("/image/:spec/:url", get(generate));
+
+    let addr = "127.0.0.1:3000".parse().unwrap();
+
+    print_test_url("https://images.pexels.com/photos/1562477/pexels-photo-1562477.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260");
+    tracing::info!("listening on {}", addr);
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn generate(Path(Params { spec, url }): Path<Params>) -> Result<String, StatusCode> {
+    let url = percent_decode_str(&url).decode_utf8_lossy();
+    let spec: ImageSpec = spec.as_str().try_into().map_err(|_| StatusCode::BAD_REQUEST)?;
+    Ok(format!("url: {}\n spec: {:#?}", url, spec))
+}
+
+fn print_test_url(url: &str) {
+    use std::borrow::Borrow;
+    let spec1 = Spec::new_resize(500, 800, resize::SampleFilter::CatmullRom);
+    let spec2 = Spec::new_watermark(20, 20);
+    let spec3 = Spec::new_filter(filter::Filter::Marine);
+    let image_spec = ImageSpec::new(vec![spec1, spec2, spec3]);
+    let s: String = image_spec.borrow().into();
+    let test_image = percent_encode(url.as_bytes(), NON_ALPHANUMERIC).to_string();
+    println!("test url: http://localhost:3000/image/{}/{}",s ,test_image);
 }
